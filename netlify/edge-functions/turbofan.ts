d88047ccf42a945dbf3710c994f26420ca615d60
Danny <dnmiloen@gmail.com>
2025-09-30 19:21:47 +0700

    revert remote cache setup

--- netlify/edge-functions/turbofan.ts
+++
@@ -1,9 +0,0 @@
-// @ts-nocheck
-// eslint-disable-next-line
-export { handleRequest as default } from "https://deno.land/x/turbofan/mod.ts";
-
-export const config = {
-  method: ["GET", "PUT"],
-  path: "/v8/artifacts/:hash",
-  cache: "manual",
-};
